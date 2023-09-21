11d685c069522777d6481a8f033b73605e8ccabe
Dimasik <dimakadenuk2000@gmail.com>
2023-09-21 10:02:08 +0300

    reg/log

--- page-login.php
+++ page-login.php
@@ -1,5 +1,6 @@
 <?php session_start(); ?>
-<?php get_header(); ?>
+<?php get_header(); 
+?>
 <main>
 <ul class="tabs">
         <li class="tab-link active" data-tab="login">Login</li>
@@ -8,8 +9,8 @@
     <section id="login" class="tab-content active">
         <h1>Login</h1>
         <form method="POST" action="">
-            <input id="user_name" type="text" name="user_name" placeholder="Имя пользователя">
-            <input id="user_password" type="password" name="user_password" placeholder="Пароль">
+            <input id="user_name" type="text" name="user_name" placeholder="Имя пользователя" required>
+            <input id="user_password" type="password" name="user_password" placeholder="Пароль" required>
             <input type="submit" value="Send">
         </form>
         <?php
@@ -27,8 +28,8 @@
             // Проверка, была ли отправлена форма
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 // Получаем данные, введенные пользователем
-                $user_name = $_POST["user_name"] ?? "null";
-                $user_password = $_POST["user_password"] ?? "null";
+                $user_name = $_POST["user_name"] ?? null;
+                $user_password = $_POST["user_password"] ?? null;
 
                 // Запрос к базе данных для проверки учетных данных
                 $query = "SELECT * FROM users WHERE name = '$user_name' AND password = '$user_password'";
@@ -60,18 +61,18 @@
     <section id="register" class="tab-content">
         <h1>Register</h1>
         <form method="POST" action="">
-            <input id="register_name" type="text" name="register_name" placeholder="name">
-            <input id="register_email" type="email" name="register_email" placeholder="email">
-            <input id="register_password" type="password" name="register_password" placeholder="password">
+            <input id="register_name" type="text" name="register_name" placeholder="name" required>
+            <input id="register_email" type="email" name="register_email" placeholder="email" required>
+            <input id="register_password" type="password" name="register_password" placeholder="password" required>
             <input type="submit" value="Send">
         </form>
         <?php
             $error_message = ""; // Инициализация переменной для сообщения об ошибке
 
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $user_name = $_POST["register_name"] ?? false;
-                $user_email = $_POST["register_email"] ?? false;
-                $user_password = $_POST["register_password"] ?? false;
+                $user_name = $_POST["register_name"] ;
+                $user_email = $_POST["register_email"] ;
+                $user_password = $_POST["register_password"] ;
 
                 // Создаем соединение с базой данных и выполняем запрос на вставку данных
                 $mysql = new mysqli("localhost", "root", "root", "local");
@@ -91,27 +92,11 @@
         ?>
     </section>
 </main>
-<script>
-    document.addEventListener("DOMContentLoaded", function() {
-    const tabLinks = document.querySelectorAll(".tab-link");
-    const tabContents = document.querySelectorAll(".tab-content");
 
-    tabLinks.forEach(link => {
-        link.addEventListener("click", () => {
-            // Удалите класс 'active' у всех табов и секций
-            tabLinks.forEach(tab => tab.classList.remove("active"));
-            tabContents.forEach(content => content.classList.remove("active"));
-
-            // Добавьте класс 'active' только к выбранному табу и соответствующей секции
-            const target = link.getAttribute("data-tab");
-            document.querySelector(`#${target}`).classList.add("active");
-            link.classList.add("active");
-        });
-    });
-});
-
-</script>
-
+<?php
+get_sidebar();
+get_footer();
+?>
 
 
 
