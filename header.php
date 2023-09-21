b1176e9763877cb2dd77c4bf1a65ef74971404f2
Jane Kostevich <yana.kostevich18@gmail.com>
2023-09-21 16:50:46 +0300

    settings fonts and colors

--- header.php
+++ header.php
@@ -21,52 +21,69 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" />
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Righteous&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
 	<div id="page" class="site">
-		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'video-match'); ?></a>
-		<aside id="sidebar">
-			<header id="masthead" class="site-header">
-				<nav>
-					<div class="navbar">
-						<div class="container nav-container">
-							<input class="checkbox" type="checkbox" name="" id="" />
-							<div class="hamburger-lines">
-								<span class="line line1"></span>
-								<span class="line line2"></span>
-								<span class="line line3"></span>
-							</div>
-							<div class="logo">
-							<?php 
-									if (isset($_SESSION['user_name'])) {
-										$user_name = $_SESSION['user_name'];
-										// Теперь $user_name содержит имя пользователя
-									}
-									if (isset($_SESSION['user_id'])) {
-										$user_id = $_SESSION['user_id'];
-									}
-									
-								?>
-								<h1>VideoMatch</h1>
-								<span class="reg-name">Name:<?php echo $user_name ?></span>
-								<span class="reg-name">ID:<?php echo $user_id ?></span>
-							</div>
-							<div class="menu-items">
-								<?php
+		<div class="logo-users">
+			<div id="logo" class="logo">
+				<h1>VideoMatch</h1>
+			</div>
+			<div class="user">
+				<?php
+				if (isset($_SESSION['user_name'])) {
+					$user_name = $_SESSION['user_name'];
+				?>
+					<h5>
+						<?php echo $user_name; ?>
+					</h5>
+				<?php
+				}
+				?>
 
-								wp_nav_menu(
-									array(
-										'theme_location' => 'menu',
-										'menu_id'        => 'primary-menu',
-									)
-								);
-								?>
+				<!-- <?php
+				if (isset($_SESSION['user_name'])) {
+
+					$user_name = $_SESSION['user_name'];
+				}
+				// if (isset($_SESSION['user_id'])) {
+				// 	$user_id = $_SESSION['user_id'];
+				// }
+
+				?> -->
+			</div>
+		</div>
+		<div id="wrapper">
+			<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'video-match'); ?></a>
+			<aside id="sidebar">
+				<header id="masthead" class="site-header">
+					<nav>
+						<div class="navbar">
+							<div class="container nav-container">
+								<input class="checkbox" type="checkbox" name="" id="" />
+								<div class="hamburger-lines">
+									<span class="line line1"></span>
+									<span class="line line2"></span>
+									<span class="line line3"></span>
+								</div>
+								<div class="menu-items">
+									<?php
+
+									wp_nav_menu(
+										array(
+											'theme_location' => 'menu',
+											'menu_id'        => 'primary-menu',
+										)
+									);
+									?>
+								</div>
 							</div>
 						</div>
-					</div>
-				</nav>
-			</header><!-- #masthead -->
-		</aside>
+					</nav>
+				</header><!-- #masthead -->
+			</aside>
