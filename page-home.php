19461384e291275b96d701cea281dc9d93eeb840
Dimasik <dimakadenuk2000@gmail.com>
2023-09-22 23:09:14 +0300

    git back

--- page-home.php
+++ page-home.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 get_header();
 ?>
@@ -11,3 +12,17 @@ get_header();
 get_sidebar();
 get_footer();
 ?>
+=======
+<?php
+get_header();
+?>
+
+<main id="primary" class="site-main">
+
+    <div class="film-container"></div>
+    
+</main><!-- #main -->
+<?php
+get_sidebar();
+get_footer();
+>>>>>>> b5964a3070cc6c6b8bb2b0f4ad6ad9b2f0a1fa25
--- page-likes.php
+++ page-likes.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 get_header();
 ?>
@@ -32,4 +33,31 @@ get_header();
 <?php
 get_sidebar();
 get_footer();
+=======
+<?php
+get_header();
+?>
+
+<main id="primary" class="site-main">
+    <div id="movie-container">
+        <div class="movie-card">
+            <img src="" alt="Movie Poster" id="movie-poster">
+            <h1 id="movie-title"></h1>
+            <i class="fas fa-info-circle"></i>
+            <p id="movie-overview"></p>
+        </div>
+        <div class="navigation-swipe">
+            <button id="close-button">Хуйня</button>
+            <button id="like-button">Збс</button>
+            
+        </div>
+    </div>
+    
+
+</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
+>>>>>>> b5964a3070cc6c6b8bb2b0f4ad6ad9b2f0a1fa25
 ?>
