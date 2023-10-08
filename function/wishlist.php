29212d89fb6341d3e8b28b8c9773bac766c55aed
Jane Kostevich <yana.kostevich18@gmail.com>
2023-10-08 19:58:33 +0300

    name films wishlist

--- function/wishlist.php
+++ function/wishlist.php
@@ -17,11 +17,25 @@
 
     // Выполнение запроса
     $result = mysqli_query($connection, $query);
-
     if ($result) {
         // Выводим movie_id в виде списка <li>
         while ($row = mysqli_fetch_assoc($result)) {
-            echo "<li class='wishlist-movie'>{$row['movie_id']}</li>";
+            $movie_id = $row['movie_id'];
+
+            // Виконуємо запит до  API, щоб отримати інформацію про фільм за його ID
+            $api_key = "19cc2d55ec287216302aaf07144d9835";
+            $api_url = "https://api.themoviedb.org/3/movie/$movie_id?api_key=$api_key";
+
+            // Виконуємо запит до API
+            $api_response = file_get_contents($api_url);
+
+            // Парсимо відповідь API
+            $api_data = json_decode($api_response, true);
+
+            // Виводимо назву фільму, якщо вона доступна
+            if (isset($api_data['title'])) {
+                echo "<li class='wishlist-movie'>" . $api_data['title'] . "</li>";
+            }
         }
     } else {
         echo "Ошибка при выполнении запроса: " . mysqli_error($connection);
