4f002dc44be6086e6dca1d4ba1f6b99c8c7e39e3
FlexxAntihype <dimakadenuk2000@gmail.com>
2023-11-29 23:39:27 +0200

    search and rename no profit

--- function/rename.php
+++ function/rename.php
@@ -1,16 +1,45 @@
-<?php 
+<?php
 session_start();
 
 
+$mysql = new mysqli("localhost", "root", "", "local");
+$mysql->query("SET NAMES 'UTF8'");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["rename_user"])) {
+        $new_username = $_POST["rename_user"];
+    
+        if (isset($_SESSION["user_id"])) {
+            $user_id = $_SESSION["user_id"];
+    
+            echo "User ID: $user_id";
+    
+            $update_query = "UPDATE users SET name = ? WHERE id = ?";
+            $stmt = $mysql->prepare($update_query);
+    
+            if ($stmt) {
+                $stmt->bind_param("si", $new_username, $user_id);
+                $stmt->execute();
+    
+                // После успешного выполнения запроса
+                $_SESSION["name"] = $new_username;
+    
+                $stmt->close();
+            } else {
+                echo "Error in preparing statement: " . $mysql->error;
+            }
+        } else {
+            echo "User ID not found in session";
+        }
+    }
+    
 
 ?>
 
-
 <section class="rename_account">
-        <h3>Rename account</h3>
-        <form method="POST" action="" name="rename">
+    <h3>Rename account</h3>
+    <form method="POST" action="" name="rename">
         <label for="">Введите новое имя пользователя</label>
         <input type="text" name="rename_user" placeholder="Введите новое имя пользователя">
         <input type="submit" name="rename">
-        </form>
+    </form>
 </section>
--- function/search-users.php
+++ function/search-users.php
@@ -4,42 +4,36 @@ session_start();
 $mysql = new mysqli("localhost", "root", "", "local");
 $mysql->query("SET NAMES 'UTF8'");
 
-$found_user = "";
+$found_user_id = "";
 
-$found_user = $_POST["search_user"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["search_user"])) {
+        $search_user_id = $_POST["search_user"];
 
+        $query = "SELECT * FROM users WHERE id = $search_user_id";
+        $result = $mysql->query($query);
 
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $found_user_id = $row['id'];
+            $_SESSION['found_user_id'] = $found_user_id;
+        } else {
+            $found_user_id = 'User с таким ID не найден';
+            $_SESSION['found_user_id'] = $found_user_id;
+        }
+    }
+}
 ?>
 
-
 <section class="section_search_users">
-        <h3>Search Users</h3>
-        <form id="search_users" method="POST" action="" name="search_users">
+    <h3>Search Users</h3>
+    <h4>User ID:
+        <?php echo esc_html($found_user_id); ?>
+    </h4>
+    <form id="search_users" method="POST" action="" name="search_users">
         <label for="">Введите ID пользователя</label>
-        <input type="text" name="search_user" placeholder="Введите ID пользователя" value="<?php echo $search_user_id; ?>">
+        <input type="text" name="search_user" placeholder="Введите ID пользователя"
+            value="<?php echo $found_user_id ?>">
         <input type="submit" name="search_users">
-        </form>
-        <script>
-            $(document).ready(function() {
-                let form = $('#search_users');
-
-                form.on('submit', function(event) {
-                    event.preventDefault();
-                    $.ajax({
-                        type: 'POST',
-                        url: '/wp-content/themes/video-match/function/search-users.php',
-                        data: form.serialize(),
-                        success: function(response) {
-                            $('#result').html(response);
-                            console.log("Good");
-                            
-                        },
-                        error: function(error) {
-                            console.log(error);
-                        }
-                    });
-                });
-            });
-
-        </script>
+    </form>
 </section>
